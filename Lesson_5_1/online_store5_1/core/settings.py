05b1dbdfcf07a8c83429c2a4f105a7ed4aa763e0
Viacheslav Gurov <gurov>
2021-01-10 21:07:40 -0500

    dt 10 jan 2021

--- Lesson_5_1/online_store5_1/core/settings.py
+++ Lesson_5_1/online_store5_1/core/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products',
+    'clients',
     'orders',
-    'clients'
+    'products'
 ]
 
 MIDDLEWARE = [
