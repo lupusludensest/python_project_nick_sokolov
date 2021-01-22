c2a3e7c3a38365ba418816eb5dbf2f6a233f1485
Viacheslav Gurov <gurov>
2021-01-22 00:43:09 -0500

    dt 22 jan 2021

--- telegram_bot/telegram_bot/bot/urls.py
+++ telegram_bot/telegram_bot/bot/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import SubscribersList, MessagesList, MessageCreate, MessageRUD, message_list
+from .views import *
 
 
 urlpatterns = [
@@ -7,5 +7,10 @@ urlpatterns = [
     path('messages/list/', MessagesList.as_view()),
     path('messages/add/', MessageCreate.as_view()),
     path('messages/<int:pk>/', MessageRUD.as_view()),
-    path('messages/custom_list/', message_list)
+    path('messages/custom/list/', message_list),
+    path('messages/custom/add/', message_add),
+    path('messages/custom/get/<int:message_id>/', get_message),
+    path('messages/custom/update/<int:message_id>/', update_message),
+    path('messages/custom/delete/<int:message_id>/', delete_message),
+
 ]
--- telegram_bot/telegram_bot/bot/views.py
+++ telegram_bot/telegram_bot/bot/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http.response import JsonResponse
+from django.http.response import JsonResponse, HttpResponse
 from rest_framework import generics
 from .models import Subscriber, Message
 from .serializers import SubscriberListSerializer, MessageListSerializer
+import json
+import datetime
 
 
 class SubscribersList(generics.ListAPIView):
@@ -23,6 +25,7 @@ class MessageRUD(generics.RetrieveUpdateDestroyAPIView):
     queryset = Message.objects.all()
     serializer_class = MessageListSerializer
 
+
 def message_list(request):
     if request.method == 'GET':
         messages = Message.objects.all()
@@ -30,3 +33,105 @@ def message_list(request):
     else:
         data = {'status': False, 'error': 'Method is not supported'}
     return JsonResponse(data, safe=False)
+
+
+def message_add(request):
+    status_code = 0
+    json_response = {}
+    json_response['status'] = False
+    json_response['error_message'] = ''
+    if request.method == 'POST':
+        try:
+            request_json = json.loads(request.body)
+            new_message = Message.objects.create(
+                is_sent=request_json['sent'],
+                message_text = request_json['text_message']
+            )
+            json_response['status'] = True
+            status_code = 201
+            json_response['result'] = {
+                'message_id': new_message.id,
+                'text_message': new_message.message_text,
+                'is_sAnd': new_message.is_sent
+            }
+        except BaseException:
+            json_response['error_message'] = 'Bad_request'
+            status_code = 400
+    else:
+        json_response['error_message'] = 'Method not allowed'
+        status_code = 405
+    return JsonResponse(json_response, status=status_code, safe=True)
+
+
+def get_message(request, message_id):
+    status_code = 0
+    json_response = {}
+    json_response['status'] = False
+    json_response['error_message'] = ''
+    if request.method == 'GET':
+        try:
+            message = Message.objects.get(id=message_id)
+            json_response['result'] = {
+                'message_id': message.id,
+                'text_message': message.message_text,
+                'is_sAnd': message.is_sent
+            }
+            json_response['status'] = True
+            status_code = 201
+        except BaseException:
+            json_response['error_message'] = 'Bad_request'
+            status_code = 400
+    else:
+        json_response['error_message'] = 'Method not allowed'
+        status_code = 405
+    return JsonResponse(json_response, status=status_code, safe=True)
+
+
+
+def update_message(request, message_id):
+    status_code = 0
+    json_response = {}
+    json_response['status'] = False
+    json_response['error_message'] = ''
+    if request.method in ['PUT', 'PATCH']:
+        try:
+            request_json = json.loads(request.body)
+            message = Message.objects.get(id=message_id)
+            message.message_text = request_json['message_text']
+            message.is_sent = request_json['is_sent']
+            message.save()
+            json_response['result'] = {
+                'message_id': message.id,
+                'text_message': message.message_text,
+                'VaSyA_PuPKin_SENT': message.is_sent
+            }
+            json_response['status'] = True
+            status_code = 201
+        except BaseException:
+            json_response['error_message'] = 'Bad Request'
+            status_code = 400
+    else:
+        json_response['error_message'] = 'Method Not Allowed'
+        status_code = 405
+    return JsonResponse(json_response, status=status_code, safe=True)
+
+
+
+def delete_message(request, message_id):
+    status_code = 0
+    json_response = {}
+    json_response['status'] = False
+    json_response['error_message'] = ''
+    if request.method == 'DELETE':
+        try:
+            message = Message.objects.get(id=message_id)
+            message.delete()
+            json_response['status'] = True
+            status_code = 200
+        except BaseException:
+            json_response['error_message'] = 'Bad Request'
+            status_code = 400
+    else:
+        json_response['error_message'] = 'Method Not Allowed'
+        status_code = 405
+    return JsonResponse(json_response, status=status_code, safe=True)
