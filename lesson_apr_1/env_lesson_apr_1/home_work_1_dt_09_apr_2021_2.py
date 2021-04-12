afa6bc9eea026f657a12f2d75625f845430fec21
Viacheslav Gurov <gurov>
2021-04-11 22:14:13 -0400

    dt 11 apr 2021

--- lesson_apr_1/env_lesson_apr_1/home_work_1_dt_09_apr_2021_2.py
+++ lesson_apr_1/env_lesson_apr_1/home_work_1_dt_09_apr_2021_2.py
@@ -1,33 +1,34 @@
 import requests
 import time
 import winsound
-from datetime import datetime
-duration = 1000  # milliseconds
 freq = 440  # Hz
+duration = 2000  # milliseconds
 
 bitcoin_api_url = 'https://api.bittrex.com/v3/markets/BTC-USD/ticker'
 
-# def get_latest_bitcoin_price():
-#     response = requests.get(bitcoin_api_url)
-#     response_json = response.json()
-#     return f"{response_json['symbol']}: {float(response_json['lastTradeRate'])}"
-
-def text_from():
+def btn_rate():
     response = requests.get(bitcoin_api_url)
     response_json = response.json()
     return float(response_json['lastTradeRate'])
 
-# Eternal loop
-bitcoin_history = []
-while True:
-    date = datetime.now()
-    bitcoin_history.append({'date': date, 'price': text_from()})
-    print(bitcoin_history[-1].values()) # how to compare bitcoin_history.element[-1] with bitcoin_history.element[-2]
-    # print(bitcoin_history, type(bitcoin_history))
-    # if False:
-    #     winsound.Beep(freq, duration)
-    time.sleep(20)
+def price_tracking():
+    starting_price = 0
+    while True:
+        response = requests.get(bitcoin_api_url)
+        if response.ok:
+            current_price = btn_rate()
+            if starting_price == 0:
+                starting_price = current_price
+                print(f'Current price: "{current_price}"')
+            elif starting_price >= current_price:
+                print(f'Current price does not grow.')
+            elif starting_price < current_price:
+                winsound.Beep(freq, duration)
+                print(f'Current price "{current_price}" > Starting price, price rised for "{round((current_price - starting_price), 2)}"')
+        else:
+            print(f'Server responce code "{response.status_code}"')
+        time.sleep(20)
 
-print(bitcoin_history)
+print(price_tracking())
 
 
--- lesson_apr_1/env_lesson_apr_1/temporary.py
+++ lesson_apr_1/env_lesson_apr_1/temporary.py
@@ -1,30 +1,38 @@
 import requests
 import time
-import winsound
-from datetime import datetime
-duration = 1000  # milliseconds
-freq = 440  # Hz
+import simpleaudio
+import sys
 
-bitcoin_api_url = 'https://api.bittrex.com/v3/markets/BTC-USD/ticker'
+class CryptoTracker:
 
-# def get_latest_bitcoin_price():
-#     response = requests.get(bitcoin_api_url)
-#     response_json = response.json()
-#     return f"{response_json['symbol']}: {float(response_json['lastTradeRate'])}"
+    def __init__(self, currencies="BTC-USD"):
+        self.currencies_pair = currencies
+        self.END_POINT = f"https://api.bittrex.com/v3/markets/{self.currencies_pair}/ticker"
+        self.initial_price = 0
 
-def text_from():
-    response = requests.get(bitcoin_api_url)
-    response_json = response.json()
-    return float(response_json['lastTradeRate'])
+    def track_changes(self):
+        print(f"The script let you know if the price for {self.currencies_pair} will go up.")
+        while True:
+            response = requests.get(self.END_POINT)
+            if response.ok:
+                new_price = float(response.json()['lastTradeRate'])
+                if self.initial_price == 0:
+                    self.initial_price = new_price
+                    print(f"Starting price is {new_price}")
+                elif self.initial_price >= new_price:
+                    print("Please be patient, the price is the same or getting lower (update every 30 sec.)")
+                elif self.initial_price < new_price:
+                    print(
+                        f"The price for {self.currencies_pair} went up by {round(new_price - self.initial_price, 2)}. "
+                        f"The new price is: {new_price}")
+                    sound = simpleaudio.WaveObject.from_wave_file("wake.wav").play()
+                    sound.wait_done()
+                self.initial_price = new_price
+            else:
+                print(f"Something went wrong. Response code = {response.status_code}")
+            time.sleep(30)
 
-# Eternal loop
-bitcoin_history = []
-while True:
-    date = datetime.now()
-    bitcoin_history.append({'date': date, 'price': text_from()})
-    print(bitcoin_history[-1].values())
-    # if False:
-    #     winsound.Beep(freq, duration)
-    time.sleep(20)
-
-
+if len(sys.argv) > 1:
+    CryptoTracker(sys.argv[1]).track_changes()
+else:
+    CryptoTracker().track_changes()
