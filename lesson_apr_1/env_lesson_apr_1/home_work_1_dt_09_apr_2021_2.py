c19545626739ddd4cd44d6741b2d27f44c95b91d
Viacheslav Gurov <gurov>
2021-04-12 21:20:48 -0400

    dt 12 apr 2021

--- lesson_apr_1/env_lesson_apr_1/home_work_1_dt_09_apr_2021_2.py
+++ lesson_apr_1/env_lesson_apr_1/home_work_1_dt_09_apr_2021_2.py
@@ -9,8 +9,8 @@ def price_tracking():
         bitcoin_api_url = 'https://api.bittrex.com/v3/markets/BTC-USD/ticker'
         response = requests.get(bitcoin_api_url)
         response_json = response.json()
-        if response.ok:
-            current_price = float(response_json['lastTradeRate'])
+        if response.ok: # A boolean indicating whether the response was successful (status in the range 200–299) or not
+            current_price = float(response_json['lastTradeRate']) # 'lastTradeRate' is a key used as index
             if starting_price == 0:
                 starting_price = current_price
                 print(f'Current price: "{current_price}"')
@@ -18,10 +18,10 @@ def price_tracking():
                 print(f'Current price does not grow or failed for "{round((starting_price - current_price), 2)}"')
             elif starting_price < current_price:
                 winsound.Beep(freq, duration)
-                print(f'Current price "{current_price}" is more than previous price, it rised for "{round((current_price - starting_price), 2)}"')
+                print(f'Current price "{current_price}" is more than previous price, it rised by "{round((current_price - starting_price), 2)}"')
             starting_price = current_price
         else:
-            print(f'Server responce code "{response.status_code}"')
+            print(f'Server responce code "{response.status_code}"') # returns a number that indicates the status (200 is OK, 404 is Not Found).
         time.sleep(20)
 
 print(price_tracking())
