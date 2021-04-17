586a8be9ac7d7e812474b525361b654bec074474
Viacheslav Gurov <gurov>
2021-04-16 21:47:35 -0400

    dt 16 apr 2021

--- lesson_apr_1/home_work_2_dt_14_apr_2021_1.py
+++ lesson_apr_1/home_work_2_dt_14_apr_2021_1.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
+import winsound
+freq, duration  = 380, 2000   # Hz/milliseconds
 
 class Car:
     def __init__(self, make, model, year, color, doors, ext_type, drive, gas_hybr_elect, horse_power, price, max_passangers):
-        self.car_make, self.car_model, self.car_year, self.car_color, self.car_doors, self.car_ext_type, self.car_drive, self.car_gas_hybr_elect, self.car_horse_power, self.car_price, self.max_passangers, self.car_passangers  \
+        self.car_make, self.car_model, self.car_year, self.car_color, self.car_doors, self.car_ext_type, self.car_drive, self.car_gas_hybr_elect, self.car_horse_power, self.car_price, self.max_passangers, self.car_passangers \
         = make, model, year, color, doors, ext_type, drive, gas_hybr_elect, horse_power, price, max_passangers, 0
+        self.car_pssngrs_as_prsns = []
         print(f'Car: {make}, {model}, year:{year}, {color}, doors:{doors}, {ext_type}, {drive}, {gas_hybr_elect}, horse power:{horse_power}, price: ${price}, max passangers: {max_passangers}\n')
 
     def ordered(self):
@@ -21,6 +24,20 @@ class Car:
     def check_passangers(self):
         print(f'There are "{int(self.car_passangers)}" passangers are in the {self.car_make}/{self.car_model}/year:{self.car_year}/{self.car_color}/doors:{self.car_doors}/exterior type:{self.car_ext_type}/{self.car_drive}/{self.car_gas_hybr_elect}/horse power:{self.car_horse_power}/${self.car_price}/max.pass.:{self.max_passangers} ')
 
+    def check_pssngrs_as_prsns(self):
+        print(f'There are "{int(len(self.car_pssngrs_as_prsns))}" passangers are in the {self.car_make}/{self.car_model}/year:{self.car_year}/{self.car_color}/doors:{self.car_doors}/exterior type:{self.car_ext_type}/{self.car_drive}/{self.car_gas_hybr_elect}/horse power:{self.car_horse_power}/${self.car_price}/max.pass.:{self.max_passangers} ')
+
+    def add_pssngrs_as_prsns(self, psngr_nm):
+        if len(self.car_pssngrs_as_prsns) + 1  < self.max_passangers:
+            mssg = f'{psngr_nm} got the car {self.car_make}/{self.car_model}/year:{self.car_year}/{self.car_color}/doors:{self.car_doors}/exterior type:{self.car_ext_type}/{self.car_drive}/{self.car_gas_hybr_elect}/horse power:{self.car_horse_power}/${self.car_price}/max.pass.:{self.max_passangers} '
+            self.car_pssngrs_as_prsns.append(psngr_nm)
+        elif len(self.car_pssngrs_as_prsns) + 1  == self.max_passangers:
+            mssg = f'{psngr_nm} got the car, but no more space! {self.car_make}/{self.car_model}/year:{self.car_year}/{self.car_color}/doors:{self.car_doors}/exterior type:{self.car_ext_type}/{self.car_drive}/{self.car_gas_hybr_elect}/horse power:{self.car_horse_power}/${self.car_price}/max.pass.:{self.max_passangers} '
+            self.car_pssngrs_as_prsns.append(psngr_nm)
+        elif len(self.car_pssngrs_as_prsns) + 1  > self.max_passangers:
+            mssg = f'No more space! in {self.car_make}/{self.car_model}/year:{self.car_year}/{self.car_color}/doors:{self.car_doors}/exterior type:{self.car_ext_type}/{self.car_drive}/{self.car_gas_hybr_elect}/horse power:{self.car_horse_power}/${self.car_price}/max.pass.:{self.max_passangers} '
+        return print(mssg)
+
     def passangers_get_in(self, psngrs_qntty = int(input('Enter quantity of the passangers: \n'))):
         if psngrs_qntty <= self.max_passangers:
             print_out = (f'{psngrs_qntty} passangers are in the {self.car_make}/{self.car_model}/year:{self.car_year}/{self.car_color}/doors:{self.car_doors}/exterior type:{self.car_ext_type}/{self.car_drive}/{self.car_gas_hybr_elect}/horse power:{self.car_horse_power}/${self.car_price}/max.pass.:{self.max_passangers}')
@@ -29,6 +46,7 @@ class Car:
             print_out = (f'Max passangers! {psngrs_qntty} passanger/s is/are in the {self.car_make}/{self.car_model}/year:{self.car_year}/{self.car_color}/doors:{self.car_doors}/exterior type:{self.car_ext_type}/{self.car_drive}/{self.car_gas_hybr_elect}/horse power:{self.car_horse_power}/${self.car_price}/max.pass.:{self.max_passangers}')
             self.car_passangers += self.max_passangers
         elif psngrs_qntty > self.max_passangers:
+            winsound.Beep(freq, duration)
             print_out = (f'No more passangers! {psngrs_qntty - self.max_passangers} passengers are excessive to the  {self.car_make}/{self.car_model}/year:{self.car_year}/{self.car_color}/doors:{self.car_doors}/exterior type:{self.car_ext_type}/{self.car_drive}/{self.car_gas_hybr_elect}/horse power:{self.car_horse_power}/${self.car_price}/max.pass.:{self.max_passangers}')
             self.car_passangers += self.max_passangers
         return print(print_out)
@@ -44,8 +62,16 @@ car2.discount_price()
 
 car3 = Car('Tesla', 'Model X', 2021, 'Yellow', 5, 'SUV', 'AWD', 'Electro', 360, 89990, 6)
 car3.ordered()
-car3.passangers_get_in()
-car3.check_passangers()
+# car3.passangers_get_in()
+# car3.check_passangers()
+car3.add_pssngrs_as_prsns('Foo')
+car3.add_pssngrs_as_prsns('Booo')
+car3.add_pssngrs_as_prsns('Roooo')
+car3.add_pssngrs_as_prsns('Woo')
+car3.add_pssngrs_as_prsns('Mooo')
+car3.add_pssngrs_as_prsns('Toooo')
+car3.add_pssngrs_as_prsns('Qoooo0')
+car3.check_pssngrs_as_prsns()
 
 
 
