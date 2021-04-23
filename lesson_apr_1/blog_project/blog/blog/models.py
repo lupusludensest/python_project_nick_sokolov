47181f2ebbc6763a93fe2fe62269f127a0115ab7
Viacheslav Gurov <gurov>
2021-04-23 06:56:03 -0400

    dt 23 apr 2021

--- lesson_apr_1/blog_project/blog/blog/models.py
+++ lesson_apr_1/blog_project/blog/blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Category(models.Model):
+    class Meta:
+        db_table = 'categories'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    title = models.CharField(verbose_name='Title', max_length=200, blank=False, null=False)
+
+
+class Post(models.Model):
+    class Meta:
+        db_table = 'posts'
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    user = models.ForeignKey(User, verbose_name='Owner', on_delete=models.CASCADE)
+    title = models.CharField(verbose_name='Title', max_length=200, blank=False, null=False)
+    content = models.TextField(verbose_name='Content', blank=False, null=False)
+    time_created = models.DateTimeField(verbose_name='Created time', auto_now_add=True)
+    category = models.ForeignKey(Category, verbose_name='Category', on_delete=models.CASCADE, blank=True, null=True)
